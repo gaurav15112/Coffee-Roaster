5a7532a17bad7689fc883ebb2e3c21ce9047fe68
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-03 17:21:55 +0530

    added font and style items

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import styles from "./Navbar.module.scss";
-interface NavbarProps {
-  title: string;
-}
-
-const Navbar: React.FC<NavbarProps> = ({ title }) => {
+import Image from "next/image";
+import Link from "next/link";
+const Navbar: React.FC = () => {
   return (
     <>
       <header className="container-fluid">
         <nav className={`${styles.navbar} container`}>
-          <div className="navbar_logo">{title}</div>
+          <div className="navbar_logo">
+            <Image
+              src="/assets/logo.png"
+              alt="coffee roasters"
+              width={300}
+              height={80}
+            />
+          </div>
           <ul className="navbar_links">
-            <li className="navbar_item">Home</li>
-            <li className="navbar_item">About</li>
-            <li className="navbar_item">Contact</li>
+            <Link href="#">
+              <li className="navbar_item">Home</li>
+            </Link>
+            <Link href="#">
+              <li className="navbar_item">About us</li>
+            </Link>
+            <Link href="#">
+              <li className="navbar_item">Create Your Plan</li>
+            </Link>
           </ul>
         </nav>
       </header>
