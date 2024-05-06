934b1560696ae2ffb718866c55e1cee5b9de8b84
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-06 18:38:25 +0530

    added html and style for heading

--- src/Components/WhyChooseUs/WhyChooseUs.tsx
+++ src/Components/WhyChooseUs/WhyChooseUs.tsx
@@ -3,8 +3,18 @@ import style from "./WhyChooseUs.module.scss";
 const WhyChooseUs: React.FC = () => {
   return (
     <>
-      <div className={`${style.WhyChooseUs} container-fluid`}>
-        <div className="container"></div>
+      <div className="container-fluid">
+        <div className="container">
+          <div className={`${style.WhyChooseUs} heading`}>
+            <div className="WhyChooseUs_heading_head">Why choose us?</div>
+            <div className="WhyChooseUs_heading_para">
+              A large part of our role is choosing which particular coffees will
+              be featured in our range.This means working closely with the best
+              coffee growers to give you a more impactful experience on every
+              level
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
