b531ab8070fd9db78a1dfeeacb697b7508139a38
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-06 17:40:37 +0530

    added whychooseus component and add starting style

---
+++ src/Components/WhyChooseUs/WhyChooseUs.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./WhyChooseUs.module.scss";
+const WhyChooseUs: React.FC = () => {
+  return (
+    <>
+      <div className={`${style.WhyChooseUs} container-fluid`}>
+        <div className="container"></div>
+      </div>
+    </>
+  );
+};
+export default WhyChooseUs;
