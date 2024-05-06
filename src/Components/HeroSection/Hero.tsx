6dc90bdbe46fb73b54e7d959276d9031214f0a67
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-06 17:07:22 +0530

    added typescript

--- src/Components/HeroSection/Hero.tsx
+++ src/Components/HeroSection/Hero.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import styles from "./Hero.module.scss";
 import { Playfair_Display_SC } from "next/font/google";
+import Button from "../Button/Button";
+
 const playfairDisplay = Playfair_Display_SC({
   weight: ["400", "700", "900"],
   subsets: ["latin"],
 });
-function Hero() {
+const Hero: React.FC = () => {
   return (
     <>
-      <div className={styles.banner}>
-        <div className={`banner_heading ${playfairDisplay.className}`}>
-          Great coffee made simple
+      <div className="container-fluid">
+        <div className={`${styles.banner} container`}>
+          <div className={`banner_heading ${playfairDisplay.className}`}>
+            Great coffee made simple
+          </div>
+          <p className="banner_paragraph">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Blanditiis, mollitia! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit.
+          </p>
+          <Button />
         </div>
-        <p className="banner_paragraph">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis,
-          mollitia! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
       </div>
     </>
   );
-}
+};
 
 export default Hero;
