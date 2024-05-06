009e05c1682be42408507fc375a0698206b9d00d
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-06 15:23:31 +0530

    added more style and write html

--- src/Components/OurCollection/OurCollection.tsx
+++ src/Components/OurCollection/OurCollection.tsx
@@ -1,9 +1,85 @@
 import React from "react";
 import style from "./OurCollection.module.scss";
+import Image from "next/image";
 const OurCollection = () => {
   return (
     <>
-      <div className={`${style.ourcollect} container`}>Our Collection</div>;
+      <div className={`${style.ourcollect} container-fluid`}>
+        <div className="container">
+          <div className="ourcollect_heading">Our Collection</div>
+
+          <div className="ourcollect_card-container">
+            <div className="ourcollect_card-container_card">
+              <div className="ourcollect_card_container_card_image">
+                <Image
+                  src="/assets/a17.png"
+                  width={200}
+                  height={200}
+                  alt="image"
+                />
+              </div>
+              <div className="ourcollect_card_container_card_text">
+                Gran Sepresso
+              </div>
+              <div className="ourcollect_card_container_card_content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </div>
+            </div>
+            <div className="ourcollect_card-container_card">
+              <div className="ourcollect_card_container_card_image">
+                <Image
+                  src="/assets/a17.png"
+                  width={200}
+                  height={200}
+                  alt="image"
+                />
+              </div>
+              <div className="ourcollect_card_container_card_text">
+                Gran Sepresso
+              </div>
+              <div className="ourcollect_card_container_card_content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </div>
+            </div>
+            <div className="ourcollect_card-container_card">
+              <div className="ourcollect_card_container_card_image">
+                <Image
+                  src="/assets/a17.png"
+                  width={200}
+                  height={200}
+                  alt="image"
+                />
+              </div>
+              <div className="ourcollect_card_container_card_text">
+                Gran Sepresso
+              </div>
+              <div className="ourcollect_card_container_card_content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </div>
+            </div>
+            <div className="ourcollect_card-container_card">
+              <div className="ourcollect_card_container_card_image">
+                <Image
+                  src="/assets/a17.png"
+                  width={200}
+                  height={200}
+                  alt="image"
+                />
+              </div>
+              <div className="ourcollect_card_container_card_text">
+                Gran Sepresso
+              </div>
+              <div className="ourcollect_card_container_card_content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
