b7e484808befe4882a2647d027c0a455e504794e
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-07 15:32:41 +0530

    adding style

--- src/Components/WhyChooseUs/WhyChooseUs.tsx
+++ src/Components/WhyChooseUs/WhyChooseUs.tsx
@@ -20,41 +20,50 @@ const WhyChooseUs: React.FC = () => {
           </div>
           <div className="WhyChooseUs_section">
             <div className="WhyChooseUs_section_card">
-              <div className="container">
+              <div className="WhyChooseUs_section_card_container">
                 <div className="logo">
                   <Image src={beanlogo} alt="beanlogo" />
                 </div>
-                <div className="WhyChooseUs_section_card_body">
-                  <h5 className="card-title">Best quality</h5>
-                  <p className="card-text">
+
+                <div className="card-title">Best quality</div>
+                <div className="card-text">
+                  <p>
                     Discover an endless variety of the world s best artisan
                     coffee from each of our roasters.
                   </p>
                 </div>
               </div>
             </div>
+
             <div className="WhyChooseUs_section_card">
-              <div className="logo">
-                <Image src={giftpack} alt="logo" />
-              </div>
-              <div className="WhyChooseUs_section_card_body">
-                <h5 className="card-title">Exclusive benefits</h5>
-                <p className="card-text">
-                  Specilal offers and swag when you subscribe including 30% off
-                  your first shipment.
-                </p>
+              <div className="WhyChooseUs_section_card_container">
+                <div className="logo">
+                  <Image src={giftpack} alt="logo" />
+                </div>
+
+                <div className="card-title">Exclusive benefits</div>
+                <div className="card-text">
+                  <p>
+                    Specilal offers and swag when you subscribe including 30%
+                    off your first shipment.
+                  </p>
+                </div>
               </div>
             </div>
+
             <div className="WhyChooseUs_section_card">
-              <div className="logo">
-                <Image src={truckvan} alt="logo" />
-              </div>
-              <div className="WhyChooseUs_section_card_body">
-                <h5 className="card-title">Free shipping</h5>
-                <p className="card-text">
-                  We cover the cost and coffee is delivered fast.Peak freshness
-                  guaranteed
-                </p>
+              <div className="WhyChooseUs_section_card_container">
+                <div className="logo">
+                  <Image src={truckvan} alt="logo" />
+                </div>
+
+                <div className="card-title">Free shipping</div>
+                <div className="card-text">
+                  <p>
+                    We cover the cost and coffee is delivered fast.Peak
+                    freshness guaranteed
+                  </p>
+                </div>
               </div>
             </div>
           </div>
