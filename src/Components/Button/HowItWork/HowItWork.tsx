049f0bec448ae51d813766b6f89aeeeaff805298
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-08 17:43:56 +0530

    added button

--- src/Components/Button/HowItWork/HowItWork.tsx
+++ src/Components/Button/HowItWork/HowItWork.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import style from "./HowItWork.module.scss";
+import Button from "../Button";
 const HowItWork = () => {
   return (
     <>
@@ -24,7 +25,56 @@ const HowItWork = () => {
               {/* <hr className="howitworks_section_steps__line" /> */}
             </div>
           </div>
-          <div className="howitworks_plans"></div>
+          <div className="howitworks_plans">
+            <div className="howitworks_plans__card">
+              <div className="howitworks_plans__card_title">
+                <h2>01</h2>
+              </div>
+              <div className="howitworks_plans__card_header">
+                <h3> Pick your coffee</h3>
+              </div>
+              <div className="howitworks_plans__card_content">
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since .
+                </p>
+              </div>
+            </div>
+            <div className="howitworks_plans__card">
+              <div className="howitworks_plans__card_title">
+                <h2>02</h2>
+              </div>
+              <div className="howitworks_plans__card_header">
+                <h3> Choose the frequency</h3>
+              </div>
+              <div className="howitworks_plans__card_content">
+                <p>
+                  It has survived not only five centuries, but also the leap
+                  into electronic typesetting, remaining essentially unchanged.
+                  It was popularised
+                </p>
+              </div>
+            </div>
+            <div className="howitworks_plans__card">
+              <div className="howitworks_plans__card_title">
+                <h2>03</h2>
+              </div>
+              <div className="howitworks_plans__card_header">
+                <h3>Receive and enjoy!</h3>
+              </div>
+              <div className="howitworks_plans__card_content">
+                <p>
+                  Contrary to popular belief, Lorem Ipsum is not simply random
+                  text. It has roots in a piece of classical Latin literature
+                  from 45 BC, making it over 2000 years old.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="howitworks_button">
+            <Button buttonText="create your plan" />
+          </div>
         </div>
       </div>
     </>
