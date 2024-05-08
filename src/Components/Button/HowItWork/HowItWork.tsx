cc03f681005ee5901033b1d29472d28da2b4ace9
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-08 10:05:59 +0530

    created plan section in how it works

--- src/Components/Button/HowItWork/HowItWork.tsx
+++ src/Components/Button/HowItWork/HowItWork.tsx
@@ -10,6 +10,21 @@ const HowItWork = () => {
               <p> How it works</p>
             </div>
           </div>
+          <div className="howitworks_section">
+            <div className="howitworks_section_steps">
+              <div className="howitworks_section_steps__ellipse"></div>
+              <hr className="howitworks_section_steps__line" />
+            </div>
+            <div className="howitworks_section_steps">
+              <div className="howitworks_section_steps__ellipse"></div>
+              <hr className="howitworks_section_steps__line" />
+            </div>
+            <div className="howitworks_section_steps">
+              <div className="howitworks_section_steps__ellipse"></div>
+              {/* <hr className="howitworks_section_steps__line" /> */}
+            </div>
+          </div>
+          <div className="howitworks_plans"></div>
         </div>
       </div>
     </>
