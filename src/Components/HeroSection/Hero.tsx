2a5ae0c379be91dc20ca7cc0f28519a0d8de6a9e
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-10 15:18:19 +0530

    added props

--- src/Components/HeroSection/Hero.tsx
+++ src/Components/HeroSection/Hero.tsx
@@ -2,30 +2,29 @@ import React from "react";
 import styles from "./Hero.module.scss";
 import { Playfair_Display_SC } from "next/font/google";
 import Button from "../Button/Button";
-
+import componentdata from "../../../public/data.json";
+import { log } from "console";
 const playfairDisplay = Playfair_Display_SC({
   weight: ["400", "700", "900"],
   subsets: ["latin"],
 });
-const Hero: React.FC = () => {
+// const { herosection } = componentdata;
+// console.log(herosection);
+const Hero: React.FC = (props) => {
+  console.log(props);
+  const { buttontext } = props.data;
   return (
-    <>
-      <div className="container-fluid">
-        <div className={`${styles.banner} container`}>
-          <div className="banner_inner">
-            <div className={`banner_heading ${playfairDisplay.className}`}>
-              Great coffee made simple
-            </div>
-            <p className="banner_paragraph">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Blanditiis, mollitia! Lorem ipsum dolor sit amet consectetur
-              adipisicing elit.
-            </p>
-            <Button buttonText="Create your plan" />
+    <div className="container-fluid">
+      <div className={`${styles.banner} container`}>
+        <div className="banner_inner">
+          <div className={`banner_heading ${playfairDisplay.className}`}>
+            {props.data.title}
           </div>
+          <p className="banner_paragraph">{props.data.description}</p>
+          {buttontext ? <Button buttonText={buttontext} /> : ""}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
