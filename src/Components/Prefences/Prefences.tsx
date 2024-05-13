8ae9f4e6ec9a42067b546b4b778d26843bf33a91
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-13 11:43:42 +0530

    adde jsx and intial styling

---
+++ src/Components/Prefences/Prefences.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./Prefences.module.scss";
+const Prefences = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="container">
+          <div className={`${style.prefences}`}>preefencd</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Prefences;
