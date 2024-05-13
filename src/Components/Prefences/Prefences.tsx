02afe09b7fd6aa8edc2bef0b96dc723b3c6d14a0
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-13 13:18:09 +0530

    added elements

--- src/Components/Prefences/Prefences.tsx
+++ src/Components/Prefences/Prefences.tsx
@@ -5,7 +5,33 @@ const Prefences = () => {
     <>
       <div className="container-fluid">
         <div className="container">
-          <div className={`${style.prefences}`}>preefencd</div>
+          <div className={`${style.prefences}`}>
+            <div className="prefences_table">
+              <div className="prefences_table_title">01</div>
+              <div className="prefences_table_heading">Preferences</div>
+            </div>
+            <div className="prefences_table_line"></div>
+            <div className="prefences_table">
+              <div className="prefences_table_title">02</div>
+              <div className="prefences_table_heading">Bean Type</div>
+            </div>
+            <div className="prefences_table_line"></div>
+            <div className="prefences_table">
+              <div className="prefences_table_title">03</div>
+              <div className="prefences_table_heading">Quanity</div>
+            </div>
+            <div className="prefences_table_line"></div>
+            <div className="prefences_table">
+              <div className="prefences_table_title">04</div>
+              <div className="prefences_table_heading">Grind Option</div>
+            </div>
+            <div className="prefences_table_line"></div>
+            <div className="prefences_table">
+              <div className="prefences_table_title">05</div>
+              <div className="prefences_table_heading">Deliveries</div>
+            </div>
+            <div className="prefences_table_line"></div>
+          </div>
         </div>
       </div>
     </>
