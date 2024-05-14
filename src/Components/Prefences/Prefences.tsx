76260a9c6a7d1239c6d246e42beea62b34cf3552
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-14 18:38:03 +0530

    add conditions

--- src/Components/Prefences/Prefences.tsx
+++ src/Components/Prefences/Prefences.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Prefences.module.scss";
+import arrow from "../../../public/assets/Vector.svg";
+import Image from "next/image";
+
+const data = {
+  prefences: {
+    title: "Great coffee made simple",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since .",
+    buttontext: "create your plan",
+  },
+};
 const Prefences = () => {
+  const [isOpen, setOpen] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  const handleArrow = () => {
+    setOpen(!isOpen);
+    console.log(!isOpen);
+  };
+  const handleCardClick = () => {
+    setClicked(!clicked);
+  };
   return (
     <>
       <div className="container-fluid">
@@ -46,35 +67,55 @@ const Prefences = () => {
 
             <div className="prefences_secondCont">
               <div className="prefences_secondCont_card">
-                <div className="prefences_secondCont_card__heading">
-                  How do you drink your coffee?
-                </div>
-                <div className="prefences_secondCont_secondConts">
-                  <div className="prefences_secondCont_secondConts_cards">
-                    <div className="prefences_secondCont_secondConts_cards_title">
-                      Capsule
-                    </div>
-                    <div className="prefences_secondCont_secondConts_cards_content">
-                      Compatible with Nespresso systems and similar brewers
-                    </div>
-                  </div>
-                  <div className="prefences_secondCont_secondConts_cards">
-                    <div className="prefences_secondCont_secondConts_cards_title">
-                      Filter
-                    </div>
-                    <div className="prefences_secondCont_secondConts_cards_content">
-                      Compatible with Nespresso systems and similar brewers
+                {
+                  <div
+                    onClick={handleArrow}
+                    className="prefences_secondCont_card__heading"
+                  >
+                    How do you drink your coffee?
+                    <div
+                      className={`prefences_secondCont_card__arrow ${
+                        isOpen ? "rotateup" : ""
+                      }`}
+                    >
+                      <Image src={arrow} alt="arrow" />
                     </div>
                   </div>
-                  <div className="prefences_secondCont_secondConts_cards">
-                    <div className="prefences_secondCont_secondConts_cards_title">
-                      Espresso
+                }
+                {isOpen && (
+                  <div className="prefences_secondCont_secondConts">
+                    <div
+                      onClick={handleCardClick}
+                      className={`prefences_secondCont_secondConts_cards ${
+                        clicked ? "clicked" : ""
+                      }`}
+                    >
+                      <div className="prefences_secondCont_secondConts_cards_title">
+                        Capsule
+                      </div>
+                      <div className="prefences_secondCont_secondConts_cards_content">
+                        Compatible with Nespresso systems and similar brewers
+                      </div>
                     </div>
-                    <div className="prefences_secondCont_secondConts_cards_content">
-                      Compatible with Nespresso systems and similar brewers
+
+                    <div className="prefences_secondCont_secondConts_cards">
+                      <div className="prefences_secondCont_secondConts_cards_title">
+                        Filter
+                      </div>
+                      <div className="prefences_secondCont_secondConts_cards_content">
+                        Compatible with Nespresso systems and similar brewers
+                      </div>
+                    </div>
+                    <div className="prefences_secondCont_secondConts_cards">
+                      <div className="prefences_secondCont_secondConts_cards_title">
+                        Espresso
+                      </div>
+                      <div className="prefences_secondCont_secondConts_cards_content">
+                        Compatible with Nespresso systems and similar brewers
+                      </div>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
             </div>
           </div>
@@ -83,5 +124,5 @@ const Prefences = () => {
     </>
   );
 };
-
+const pref = () => {};
 export default Prefences;
