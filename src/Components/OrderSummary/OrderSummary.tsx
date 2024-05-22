4486337b5243fdf3a51e03cc6a3d067c72847ab9
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-22 12:21:03 +0530

    added styles

---
+++ src/Components/OrderSummary/OrderSummary.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./OrderSummary.module.scss";
+const OrderSummary = () => {
+  return (
+    <>
+      <div className="container">
+        <div className={`${style.ordersummary} container-fluid`}>
+          <h2 className="ordersummary__title ">jjj</h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OrderSummary;
