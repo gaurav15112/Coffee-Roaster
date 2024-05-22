2e864ff3038800f00fc4df4d73177bcdb3bb3032
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-22 13:22:37 +0530

    added title and description

--- src/Components/OrderSummary/OrderSummary.tsx
+++ src/Components/OrderSummary/OrderSummary.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import style from "./OrderSummary.module.scss";
+
 const OrderSummary = () => {
   return (
     <>
       <div className="container">
         <div className={`${style.ordersummary} container-fluid`}>
-          <h2 className="ordersummary__title ">jjj</h2>
+          <div className="ordersummary__title ">Order Summary </div>
+          <div className="ordersummary__description">
+            I drink my coffee as <span>Filter</span> with a <span>Decaf</span>
+            type of bean. <span>250g</span> ground ala <span>Cafetiere</span>
+            sent to me <span>Every Week.</span>
+          </div>
         </div>
       </div>
     </>
