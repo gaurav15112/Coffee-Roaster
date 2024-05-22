6247ccb329c593986ffd74a3ca46674ef55e9ac0
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-22 18:32:41 +0530

    added space

--- src/Components/OrderSummary/OrderSummary.tsx
+++ src/Components/OrderSummary/OrderSummary.tsx
@@ -5,15 +5,14 @@ const OrderSummary = ({ coffee }) => {
   console.log({ coffee });
   return (
     <>
-      <div className="container">
-        <div className={`${style.ordersummary} container-fluid`}>
+      <div className={`${style.ordersummary}`}>
+        <div className={`ordersummary__container container-fluid`}>
           <div className="ordersummary__title ">ORDER SUMMARY </div>
           <div className="ordersummary__description">
             I drink my coffee as <span>{coffee.flavour}</span> with a
-            <span> {coffee.taste}</span>
-            Type of bean. <span> {coffee.quantity}</span> ground ala
-            <span>{coffee.grind}</span>
-            sent to me <span>{coffee.deliver}</span>
+            <span> {coffee.taste} </span>
+            Type of bean.<span> {coffee.quantity}</span> ground ala
+            <span>{coffee.grind}</span> sent to me <span>{coffee.deliver}</span>
           </div>
         </div>
       </div>
