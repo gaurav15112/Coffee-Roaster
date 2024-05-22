ac2c02bb54189eb4af7b700e8c353d101e4b2d64
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-22 14:30:23 +0530

    change text to uppercase

--- src/Components/OrderSummary/OrderSummary.tsx
+++ src/Components/OrderSummary/OrderSummary.tsx
@@ -6,7 +6,7 @@ const OrderSummary = () => {
     <>
       <div className="container">
         <div className={`${style.ordersummary} container-fluid`}>
-          <div className="ordersummary__title ">Order Summary </div>
+          <div className="ordersummary__title ">ORDER SUMMARY </div>
           <div className="ordersummary__description">
             I drink my coffee as <span>Filter</span> with a <span>Decaf</span>
             type of bean. <span>250g</span> ground ala <span>Cafetiere</span>
