e56ff2d280aae0720e46dbc294d18ccaed3354f4
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-05-28 12:02:34 +0530

    remove extra lines

--- src/Components/HeroSection/Hero.tsx
+++ src/Components/HeroSection/Hero.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import styles from "./Hero.module.scss";
 import { Playfair_Display_SC } from "next/font/google";
 import Button from "../Button/Button";
-import componentdata from "../../../public/data.json";
-import { log } from "console";
+
 const playfairDisplay = Playfair_Display_SC({
   weight: ["400", "700", "900"],
   subsets: ["latin"],
 });
-// const { herosection } = componentdata;
-// console.log(herosection);
+
 const Hero: React.FC = (props: any) => {
-  console.log(props);
   const { buttontext } = props?.data;
   return (
     <div className="container-fluid">
