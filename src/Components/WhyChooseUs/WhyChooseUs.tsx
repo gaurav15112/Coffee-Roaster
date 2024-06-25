c2726240929f09d8b27daae7e4c370079dae81be
gaurav15112 <113169268+gaurav15112@users.noreply.github.com>
2024-06-25 10:50:53 +0530

    removed import line

--- src/Components/WhyChooseUs/WhyChooseUs.tsx
+++ src/Components/WhyChooseUs/WhyChooseUs.tsx
@@ -4,69 +4,79 @@ import Image from "next/image";
 import beanlogo from "../../../public/assets/bxs_coffee-bean.svg";
 import giftpack from "../../../public/assets/bxs_gift.svg";
 import truckvan from "../../../public/assets/fa-solid_shipping-fast.svg";
-import data from "../../../public/data.json";
-const WhyChooseUs: React.FC = () => {
+
+import { title } from "process";
+
+interface WhyChooseUs {
+  title: string;
+  description: string;
+  cards: Array<{
+    image: string;
+    title: string;
+    description: string;
+  }>;
+}
+const data = {
+  title: "Why Choose Us",
+  description:
+    "A large part of our role is choosing which particular coffees will be featured in our range.This means working closely with the best coffee growers to give you a more impactful experience on every level",
+  cards: [
+    {
+      image: "assets/bxs_coffee-bean.svg",
+      title: "Best quality",
+      description:
+        "Discover an endless variety of the world s best artisan coffee from each of our roasters.",
+    },
+    {
+      image: "assets/bxs_gift.svg",
+      title: "Exclusive benefits",
+      description:
+        "Specilal offers and swag when you subscribe including 30% off your first shipment.",
+    },
+    {
+      image: "assets/fa-solid_shipping-fast.svg",
+      title: "Free shipping",
+      description:
+        "We cover the cost and coffee is delivered fast.Peak freshness guaranteed",
+    },
+  ],
+};
+const WhyChooseUs: React.FC<WhyChooseUs> = () => {
+  const whychooseus: WhyChooseUs = data;
   return (
     <>
       <div className="container-fluid">
         <div className={`${style.WhyChooseUs} container`}>
           <div className="WhyChooseUs_heading">
-            <div className="WhyChooseUs_heading_head">Why choose us?</div>
+            <div className="WhyChooseUs_heading_head">{whychooseus?.title}</div>
             <div className="WhyChooseUs_heading_para">
-              A large part of our role is choosing which particular coffees will
-              be featured in our range.This means working closely with the best
-              coffee growers to give you a more impactful experience on every
-              level
+              {whychooseus.description}
             </div>
           </div>
           <div className="WhyChooseUs_section">
-            <div className="WhyChooseUs_section_card">
-              <div className="WhyChooseUs_section_card_container">
-                <div className="logo">
-                  <Image src={beanlogo} alt="beanlogo" />
-                </div>
+            {whychooseus.cards.map((cards, index) => {
+              return (
+                <>
+                  <div className="WhyChooseUs_section_card">
+                    <div className="WhyChooseUs_section_card_container">
+                      <div className="logo">
+                        <Image
+                          src={cards?.image}
+                          alt={cards?.title}
+                          width={60}
+                          height={60}
+                        />
+                      </div>
 
-                <div className="card-title">Best quality</div>
-                <div className="card-text">
-                  <p>
-                    Discover an endless variety of the world s best artisan
-                    coffee from each of our roasters.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="WhyChooseUs_section_card">
-              <div className="WhyChooseUs_section_card_container">
-                <div className="logo">
-                  <Image src={giftpack} alt="logo" />
-                </div>
-
-                <div className="card-title">Exclusive benefits</div>
-                <div className="card-text">
-                  <p>
-                    Specilal offers and swag when you subscribe including 30%
-                    off your first shipment.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="WhyChooseUs_section_card">
-              <div className="WhyChooseUs_section_card_container">
-                <div className="logo">
-                  <Image src={truckvan} alt="logo" />
-                </div>
-
-                <div className="card-title">Free shipping</div>
-                <div className="card-text">
-                  <p>
-                    We cover the cost and coffee is delivered fast.Peak
-                    freshness guaranteed
-                  </p>
-                </div>
-              </div>
-            </div>
+                      <div className="card-title">{cards?.title}</div>
+                      <div className="card-text">
+                        <p>{cards?.description}</p>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
